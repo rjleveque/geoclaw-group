6fa89ad43a2ef2e10903b95ccc8a88df05072046
Paul Chamberlain <paulfromthehc@gmail.com>
2011-06-21 13:54:46 -0700

    first draft of BP01, without runup solution

---
+++ benchmarks/bp01/canonical-beach/b4step2_geo.f
@@ -0,0 +1,122 @@
+c     ============================================
+      subroutine b4step2(maxmx,maxmy,mbc,mx,my,meqn,q,
+     &            xlower,ylower,dx,dy,t,dt,maux,aux)
+c     ============================================
+c
+c     # called before each call to step
+c     # use to set time-dependent aux arrays or perform other tasks.
+c
+c     This particular routine sets negative values of q(i,j,1) to zero,
+c     as well as the corresponding q(i,j,m) for m=1,meqn.
+c     This is for problems where q(i,j,1) is a depth.
+c     This should occur only because of rounding error.
+
+c     Also calls movetopo if topography might be moving.
+
+      use geoclaw_module
+      use topo_module
+      use dtopo_module
+
+      implicit double precision (a-h,o-z)
+
+      common /comrunup/ ybar1,ybar2
+
+      dimension q(1-mbc:maxmx+mbc,1-mbc:maxmy+mbc, meqn)
+      dimension aux(1-mbc:maxmx+mbc,1-mbc:maxmy+mbc, maux)
+
+      integer istatus
+      character*25 bfname
+      double precision bt,bx
+c=====================Parameters===========================================
+
+
+        if (dx < 0.02)then
+           jbar1 = (ybar1 - ylower)/dy + 1
+           jbar2 = (ybar2 - ylower)/dy + 1
+           do i = 1,mx
+c        write(33,331) t, i, q(i,jbar,1)
+c331     format(e16.6,i5,e16.6)
+              if (q(i,jbar1,1)<0.001) then
+                  i1 = i
+              endif
+              if (q(i,jbar2,1)<0.001) then
+                  i2 = i
+              endif
+          enddo
+          runup1 = 0.5*(xlower + i1*dx)
+          runup2 = 0.5*(xlower + i2*dx)
+          write(32,40) t,runup1,runup2
+40        format(3e20.10)
+        endif
+      
+
+      bfname="../block2.tx"
+
+      if (t.lt.3.d0) then
+         open(unit=50,file=bfname,status='old',form='formatted')
+         do k=1,601
+            read(50,fmt=*,iostat=istatus) bt,bx
+            if (bt.ge. t) then  !Note: this is a bit crude...should probably interpolate between two bt
+                               !bt is the time column of block
+               xb1 = 0.01d0*bx + 0.2d0 !position of the start of the block
+               close(50)
+               exit
+            endif
+         enddo
+      else
+         xb1 = 4.045933071 + .2 !position of start of the block after stopping
+      endif
+
+      do i=1,mx
+         do j=1,my
+            x = xlower + (i-0.5d0)*dx
+            y = ylower + (j-0.5d0)*dy
+            if (abs(y).le.0.305) then
+               if ((x.ge.xb1).and.(x.le.xb1+0.91)) then !region of block
+                  aux(i,j,1) = -0.5*xb1 !block is flat on top
+               else
+                  aux(i,j,1) = -.5*x !in front of or behind block: tank bathy
+               endif
+            else
+                  aux(i,j,1) = -.5*x !to the side of the block: tank bathy
+            endif
+        enddo
+      enddo
+
+      
+
+
+c     # check for NANs in solution:
+      call check4nans(maxmx,maxmy,meqn,mbc,mx,my,q,t,1)
+
+c     # check for h < 0 and reset to zero
+c     # check for h < drytolerance
+c     # set hu = hv = 0 in all these cells
+
+      do i=1-mbc,mx+mbc
+        do j=1-mbc,my+mbc
+          if (q(i,j,1).lt.drytolerance) then
+             q(i,j,1) = max(q(i,j,1),0.d0)
+             do m=2,meqn
+                q(i,j,m)=0.d0
+                enddo
+             endif
+        enddo
+      enddo
+
+      write(26,*) 'B4STEP2: t, num_dtopo: ', t,num_dtopo
+      do i=1,num_dtopo
+          call movetopo(maxmx,maxmy,mbc,mx,my,
+     &      xlower,ylower,dx,dy,t,dt,maux,aux,
+     &      dtopowork(i0dtopo(i):i0dtopo(i)+mdtopo(i)-1),
+     &      xlowdtopo(i),ylowdtopo(i),xhidtopo(i),yhidtopo(i),
+     &      t0dtopo(i),tfdtopo(i),dxdtopo(i),dydtopo(i),dtdtopo(i),
+     &      mxdtopo(i),mydtopo(i),mtdtopo(i),mdtopo(i),
+     &      minleveldtopo(i),maxleveldtopo(i),topoaltered(i))
+      enddo
+
+
+      return
+      end
+
+
---
+++ benchmarks/bp01/canonical-beach/setrun.py
@@ -0,0 +1,328 @@
+""" 
+Module to set up run time parameters for Clawpack.
+
+The values set in the function setrun are then written out to data files
+that will be read in by the Fortran code.
+    
+""" 
+
+import os
+from pyclaw import data 
+
+
+#------------------------------
+def setrun(claw_pkg='geoclaw'):
+#------------------------------
+    
+    """ 
+    Define the parameters used for running Clawpack.
+
+    INPUT:
+        claw_pkg expected to be "geoclaw" for this setrun.
+
+    OUTPUT:
+        rundata - object of class ClawRunData 
+    
+    """ 
+    
+    assert claw_pkg.lower() == 'geoclaw',  "Expected claw_pkg = 'geoclaw'"
+
+    ndim = 2
+    rundata = data.ClawRunData(claw_pkg, ndim)
+
+    #------------------------------------------------------------------
+    # GeoClaw specific parameters:
+    #------------------------------------------------------------------
+
+    rundata = setgeo(rundata)   # Defined below
+    
+    #------------------------------------------------------------------
+    # Standard Clawpack parameters to be written to claw.data:
+    #   (or to amr2ez.data for AMR)
+    #------------------------------------------------------------------
+
+    clawdata = rundata.clawdata  # initialized when rundata instantiated
+
+
+    # Set single grid parameters first.
+    # See below for AMR parameters.
+
+
+    # ---------------
+    # Spatial domain:
+    # ---------------
+
+    # Number of space dimensions:
+    clawdata.ndim = ndim
+    
+    # Lower and upper edge of computational domain:
+    clawdata.xlower = -10.
+    clawdata.xupper = 60.
+        
+
+    # Number of grid cells:
+    clawdata.mx =  800
+    clawdata.my =  2
+    
+    dx = (clawdata.xupper - clawdata.xlower) / clawdata.mx
+    clawdata.ylower = 0.
+    clawdata.yupper = 2.*dx
+        
+
+    # ---------------
+    # Size of system:
+    # ---------------
+
+    # Number of equations in the system:
+    clawdata.meqn = 3
+
+    # Number of auxiliary variables in the aux array (initialized in setaux)
+    clawdata.maux = 3
+    
+    # Index of aux array corresponding to capacity function, if there is one:
+    clawdata.mcapa = 0
+    
+    
+    
+    # -------------
+    # Initial time:
+    # -------------
+
+    clawdata.t0 = 0.0
+    
+    
+    # -------------
+    # Output times:
+    #--------------
+
+    # Specify at what times the results should be written to fort.q files.
+    # Note that the time integration stops after the final output time.
+    # The solution at initial time t0 is always written in addition.
+
+    clawdata.outstyle = 2
+
+    if clawdata.outstyle==1:
+        # Output nout frames at equally spaced times up to tfinal:
+        clawdata.nout = 30
+        clawdata.tfinal = 120.
+
+    elif clawdata.outstyle == 2:
+        # Specify a list of output times.  
+        clawdata.tout =  [35.0, 40.0, 45.0, 50.0, 55.0, 60.0, 65.0, 70.0, 120.0]   # used if outstyle == 2
+        clawdata.nout = len(clawdata.tout)
+
+    elif clawdata.outstyle == 3:
+        # Output every iout timesteps with a total of ntot time steps:
+        iout = 1
+        ntot = 1
+        clawdata.iout = [iout, ntot]
+    
+
+
+    # ---------------------------------------------------
+    # Verbosity of messages to screen during integration:  
+    # ---------------------------------------------------
+
+    # The current t, dt, and cfl will be printed every time step
+    # at AMR levels <= verbosity.  Set verbosity = 0 for no printing.
+    #   (E.g. verbosity == 2 means print only on levels 1 and 2.)
+    clawdata.verbosity = 0
+    
+    
+
+    # --------------
+    # Time stepping:
+    # --------------
+
+    # if dt_variable==1: variable time steps used based on cfl_desired,
+    # if dt_variable==0: fixed time steps dt = dt_initial will always be used.
+    clawdata.dt_variable = 1
+    
+    # Initial time step for variable dt.  
+    # If dt_variable==0 then dt=dt_initial for all steps:
+    clawdata.dt_initial = 0.001
+    
+    # Max time step to be allowed if variable dt used:
+    clawdata.dt_max = 1e+99
+    
+    # Desired Courant number if variable dt used, and max to allow without 
+    # retaking step with a smaller dt:
+    clawdata.cfl_desired = 0.9
+    clawdata.cfl_max = 1.0
+    
+    # Maximum number of time steps to allow between output times:
+    clawdata.max_steps = 5000
+
+    
+    
+
+    # ------------------
+    # Method to be used:
+    # ------------------
+
+    # Order of accuracy:  1 => Godunov,  2 => Lax-Wendroff plus limiters
+    clawdata.order = 2
+    
+    # Transverse order for 2d or 3d (not used in 1d):
+    clawdata.order_trans = 2
+    
+    # Number of waves in the Riemann solution:
+    clawdata.mwaves = 3
+    
+    # List of limiters to use for each wave family:  
+    # Required:  len(mthlim) == mwaves
+    clawdata.mthlim = [3,3,3]
+    
+    # Source terms splitting:
+    #   src_split == 0  => no source term (src routine never called)
+    #   src_split == 1  => Godunov (1st order) splitting used, 
+    #   src_split == 2  => Strang (2nd order) splitting used,  not recommended.
+    clawdata.src_split = 1
+    
+    
+    # --------------------
+    # Boundary conditions:
+    # --------------------
+
+    # Number of ghost cells (usually 2)
+    clawdata.mbc = 2
+    
+    # Choice of BCs at xlower and xupper:
+    #   0 => user specified (must modify bcN.f to use this option)
+    #   1 => extrapolation (non-reflecting outflow)
+    #   2 => periodic (must specify this at both boundaries)
+    #   3 => solid wall for systems where q(2) is normal velocity
+    
+    clawdata.mthbc_xlower = 3
+    clawdata.mthbc_xupper = 1
+    
+    clawdata.mthbc_ylower = 3
+    clawdata.mthbc_yupper = 3
+    
+
+    # ---------------
+    # AMR parameters:
+    # ---------------
+
+
+    # max number of refinement levels:
+    mxnest = 1
+
+    clawdata.mxnest = -mxnest   # negative ==> anisotropic refinement in x,y,t
+
+    # List of refinement ratios at each level (length at least mxnest-1)
+    clawdata.inratx = [4,2,4]
+    clawdata.inraty = [2,2,2]
+
+    clawdata.inratt = [1,1,1]   
+    # Instead of setting these ratios, set the next parameter to True 
+    # to automatically choose refinement ratios in time based on estimate 
+    # of maximum wave speed on all grids at each level.
+    variable_dt_refinement_ratios = True
+
+
+    # Specify type of each aux variable in clawdata.auxtype.
+    # This must be a list of length maux, each element of which is one of:
+    #   'center',  'capacity', 'xleft', or 'yleft'  (see documentation).
+
+    clawdata.auxtype = ['center','center','yleft']
+
+
+    clawdata.tol = -1.0     # negative ==> don't use Richardson estimator
+    clawdata.tolsp = 0.5    # used in default flag2refine subroutine
+                            # (Not used in geoclaw!)
+
+    clawdata.kcheck = 3     # how often to regrid (every kcheck steps)
+    clawdata.ibuff  = 2     # width of buffer zone around flagged points
+
+    # More AMR parameters can be set -- see the defaults in pyclaw/data.py
+
+    return rundata
+    # end of function setrun
+    # ----------------------
+
+
+#-------------------
+def setgeo(rundata):
+#-------------------
+    """
+    Set GeoClaw specific runtime parameters.
+    For documentation see ....
+    """
+
+    try:
+        geodata = rundata.geodata
+    except:
+        print "*** Error, this rundata has no geodata attribute"
+        raise AttributeError("Missing geodata attribute")
+
+    # == setgeo.data values ==
+    geodata.igravity = 1
+    geodata.gravity = 1.00
+    geodata.icoordsys = 1
+
+    # == settsunami.data values ==
+    geodata.sealevel = 0.
+    geodata.drytolerance = 1.e-3
+    geodata.wavetolerance = 1.e-2
+    geodata.depthdeep = 1.e2
+    geodata.maxleveldeep = 3
+    geodata.ifriction = 0
+    geodata.coeffmanning = 0.025
+    geodata.frictiondepth = 20.
+
+    # == settopo.data values ==
+    geodata.topofiles = []
+    # for topography, append lines of the form
+    #   [topotype, minlevel, maxlevel, t1, t2, fname]
+    geodata.topofiles.append([2, 1, 1, 0., 1.e10, 'beach.topotype2'])
+
+    # == setdtopo.data values ==
+    geodata.dtopofiles = []
+    # for moving topography, append lines of the form:  (<= 1 allowed for now!)
+    #   [topotype,minlevel,maxlevel,fname]
+
+    # == setqinit.data values ==
+    geodata.iqinit = 0
+    geodata.qinitfiles = []  
+    # for qinit perturbations, append lines of the form: (<= 1 allowed for now!)
+    #   [minlev, maxlev, fname]
+    #geodata.qinitfiles.append([1, 2, 'hump.xyz'])
+
+
+    # == setregions.data values ==
+    geodata.regions = []
+    # to specify regions of refinement append lines of the form
+    #  [minlevel,maxlevel,t1,t2,x1,x2,y1,y2]
+    geodata.regions.append([2,2, 0.,1e9, 0,6, 0,1])
+    geodata.regions.append([3,3, 0.,1e9, 0,3, 0,1])
+
+    # == setgauges.data values ==
+    geodata.gauges = []
+    geodata.gauges.append([1,0.25,0,0,120])
+    geodata.gauges.append([2,9.95,0,0,120])
+    # for gauges append lines of the form  [gaugeno, x, y, t1, t2]
+
+
+    # == setfixedgrids.data values ==
+    geodata.fixedgrids = []
+    # for fixed grids append lines of the form
+    # [t1,t2,noutput,x1,x2,y1,y2,xpoints,ypoints,\
+    #  ioutarrivaltimes,ioutsurfacemax]
+
+    return rundata
+    # end of function setgeo
+    # ----------------------
+
+
+
+if __name__ == '__main__':
+    # Set up run-time parameters and write all data files.
+    import sys
+    if len(sys.argv) == 2:
+	rundata = setrun(sys.argv[1])
+    else:
+	rundata = setrun()
+
+    rundata.write()
+    
