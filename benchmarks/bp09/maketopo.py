7d9c9ce48ab0d24cc6140c0a1583037d7d71b959
Frank Gonzalez <figonzal@u.washington.edu>
2011-07-07 13:45:14 -0700

    commit changes before merging

--- benchmarks/bp09/maketopo.py
+++ benchmarks/bp09/maketopo.py
@@ -12,7 +12,8 @@ def gettopo():
     """
 
     remote_directory = 'http://kingkong.amath.washington.edu/topo/okushiri'
-    files = "AO15.tt1  MO01.tt1  OK03.tt1   OK24.tt1 MB05.tt1 OK08.tt1 HNO1993.txyz".split()
+    files = "AO15.tt1  MO01.tt1  OK03.tt1   OK24.tt1 MB05.tt1 OK08.tt1 HNO1993.txyz \
+           OK24T.tt1 OK24R.tt1 OK24B.tt1 OK24L.tt1".split()
 
     for file in files:
         topotools.get_topo(file, remote_directory)
--- benchmarks/bp09/plot_all_topo.py
+++ benchmarks/bp09/plot_all_topo.py
@@ -4,7 +4,7 @@ import geoplot
 
 
 topo_files = ['OK24.tt1', 'AO15.tt1', 'OK08.tt1', 'OK03.tt1', 'MB05.tt1', 'MO01.tt1']
-
+fname = ['OK24.tt1',  'AO15.tt1']
 for fname in topo_files:
     T = geoplot.TopoPlotData(fname)
     T.topotype=1
